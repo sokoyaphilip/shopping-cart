0a01688dcd785e8a7e75121c283448f744653eed
Sokoya Philip <sokoyaphilip@yahoo.com>
2018-05-24 15:39:04 +0100

    Add files via upload

---
+++ application/language/english/english_lang.php
@@ -0,0 +1,19 @@
+<?php
+// Website Config
+
+$lang['site_name'] = 'Shopping Cart';
+
+$lang['about_description'] = '';
+
+$lang['site_keywords'] = 'Buy, Sell, Bags, gloceries, make-up, home theater, refrigerator ...';
+$lang['site_description'] = 'Buy your goods at an affordable cost...';
+$lang['phone1'] = '08169254598';
+
+$lang['contact_email'] = 'hello@shoppingcart.ng';
+
+$lang['footer_text'] = '© '.date('Y').' shoppingcart.ng. All Rights Reserved.';
+
+// Helper
+
+$lang['ngn'] ='₦';
+?>
